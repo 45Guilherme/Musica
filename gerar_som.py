f7dd1d52f0de3dd6a7fe084e016a9978b4811e4b
Guilherme <83481058+45Guilherme@users.noreply.github.com>
2026-02-20 10:17:16 -0300

    quinto commit

--- gerar_som.py
+++ gerar_som.py
@@ -3,5 +3,9 @@ import math
 
 arquivo = wave.open("som.wav", "w")
 arquivo.setparams((1, 2, 44100, 0, "NONE", "not compressed"))
+frequencia = 440
+duracao = 2
+taxa_amostragem = 44100
+total_amostragem = int(taxa_amostragem * duracao)
 arquivo.close()
 
