323a9fb101efc91d1e80e71575644a2c69380b5d
Guilherme <83481058+45Guilherme@users.noreply.github.com>
2026-02-20 13:32:34 -0300

    sexto commit

--- gerar_som.py
+++ gerar_som.py
@@ -1,5 +1,6 @@
 import wave
 import math
+import struct
 
 arquivo = wave.open("som.wav", "w")
 arquivo.setparams((1, 2, 44100, 0, "NONE", "not compressed"))
@@ -7,5 +8,12 @@ frequencia = 440
 duracao = 2
 taxa_amostragem = 44100
 total_amostragem = int(taxa_amostragem * duracao)
+
+for i in range(total_amostragem):
+    valor = math.sin(2 * math.pi * frequencia * (i / taxa_amostragem))
+    amplitude = int(valor * 32767)
+    dados = struct.pack("<h", amplitude)
+    arquivo.writeframes(dados)
+
 arquivo.close()
 
