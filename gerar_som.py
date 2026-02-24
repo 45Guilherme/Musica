fc6f3535f673686b8ec75f7e004d75324f93e91a
Guilherme <83481058+45Guilherme@users.noreply.github.com>
2026-02-24 15:54:29 -0300

    Update

--- gerar_som.py
+++ gerar_som.py
@@ -1,6 +1,6 @@
 import wave
 
-def gerar_som(frequencia, duracao, volume, arquivo):
+def gerar_som(lista_frequencias, duracao, volume, arquivo):
     import math
     import struct
 
@@ -9,19 +9,23 @@ def gerar_som(frequencia, duracao, volume, arquivo):
 
 
     for i in range(total_amostragem):
-      valor = math.sin(2 * math.pi * frequencia * (i / taxa_amostragem))
-      amplitude = int(valor * 32767 * volume)
-      dados = struct.pack("<h", amplitude)
-      arquivo.writeframes(dados)
+         soma = 0
+         for frequencia_atual in lista_frequencias:
+             valor = math.sin(2 * math.pi * frequencia_atual * i / taxa_amostragem)
+             soma += valor
+
+         valor_final = soma / len(lista_frequencias) 
+
+         amplitude_final = int(valor_final * 32767 * volume)
+         dados = struct.pack("<h", amplitude_final)
+         arquivo.writeframes(dados)
 
 
 arquivo = wave.open("som.wav", "w")
 
 arquivo.setparams((1, 2, 44100, 0, "NONE", "not compressed"))
 
-gerar_som(440, 2, 0.5, arquivo)
-gerar_som(880, 2, 0.5, arquivo)
-gerar_som(660, 2, 0.5, arquivo)
+gerar_som([440, 660, 880], 2, 0.5, arquivo)
 
 arquivo.close() 
 
